61cd45731650aa2f7ddb372355bd17ec30e21253
Jon Whitter <jon.whitter@d2digital.co.uk>
2018-06-06 09:36:33 +0100

    added fontawesome

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import {faBed, faUserMd, faCalendarAlt} from '@fortawesome/fontawesome-free-solid';
+import fontawesome from '@fortawesome/fontawesome';
+
+fontawesome.library.add(faBed, faUserMd, faCalendarAlt);
+
 if (environment.production) {
   enableProdMode();
 }
